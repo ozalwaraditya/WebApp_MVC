0efe8f6fbd328838f8b38dbaa7720e121a09d357
Ozalwar Aditya <164456695+ozalwaraditya@users.noreply.github.com>
2025-02-13 12:22:40 +0530

    Update ApplicationDbContext.cs

--- Web.Data/Data/ApplicationDbContext.cs
+++ Web.Data/Data/ApplicationDbContext.cs
@@ -15,11 +15,7 @@ namespace Web.Data.Data
             //Seeding the data to database
         //protected override void OnModelCreating(ModelBuilder modelBuilder)
         //{
-        //    modelBuilder.Entity<User>().HasData(
-        //        new User { Id = 1, Name="Aditya", Email="aditya@gmail.com", Password = "aditya"},
-        //        new User { Id = 2, Name="Tanmay", Email="tanmay@gmail.com", Password = "tanmay"},
-        //        new User { Id = 3, Name="Martand", Email="martand@gmail.com", Password = "martand"}
-        //        );
+       
         //}
     }
 }
